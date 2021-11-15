e4703eba730e6666c0a445647b1d7dbef3eb2ab1
Amnah Asad <amnah_asad@hotmail.com>
2021-11-15 05:05:43 +0000

    Added the move commands

--- client.js
+++ client.js
@@ -4,14 +4,34 @@ const net = require("net"); //creates the client object/connection
 const name = 'Name: AA';
 // establishes a connection with the game server
 const connect = function () {
+
+    const name = 'Name: AA';
+    // const up = 'Move: up';
+    const down = 'Move: down';
+    const right = 'Move: right';
+    const left = 'Move: left';
+
     const conn = net.createConnection({
         host: '165.227.47.243', // IP address here,
         port: 50541 // PORT number here,
     });
 
-    conn.write(`${name}`);
     // interpret incoming data as text
     conn.setEncoding("utf8");
+    conn.write(`${name}`);
+
+    // setInterval(() => {
+    //     conn.write(`${up}`)}, 3000);
+
+    setInterval(() => {
+        conn.write(`${down}`)}, 3000);
+
+    setInterval(() => {
+        conn.write(`${right}`)}, 3000);
+
+    setInterval(() => {
+        conn.write(`${left}`)}, 3000);
+
 
     conn.on("connect", (data) => {
         // code that does something when the connection is first established
