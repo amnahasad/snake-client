6dabb618d77b6968aa968baede715c42c762fb7d
Amnah Asad <amnah_asad@hotmail.com>
2021-11-15 05:16:02 +0000

    Added setUpInput function

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on('data', handleUserInput)
+    return stdin;
+};
+
+
+const handleUserInput = function (data) {
+    if (data === '\u0003') {
+        process.exit();
+    }
+};
+
+module.export = { setupInput };
--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const connect = require("./client");
+const setupInput = require('./input');
 
 //This will serve as your main file through which you will launch the game client.
 
